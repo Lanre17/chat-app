cb66e34eb6a12f99dde2533380206b35ff9a4249
Lanre Yusuff <Lanre_iby@yahoo.com>
2023-02-07 03:29:34 -0500

    Deploy

--- server/index.js
+++ server/index.js
@@ -22,11 +22,11 @@ app.use("/api/messages", messageRoute);
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'client', 'dist', 'index.html')
     )
   );
 } else {
